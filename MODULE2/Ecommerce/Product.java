effb094a8cd9b0cf5e862f70fed28daa95200055
Ameenajabeen <145528572+Ameenajabeen@users.noreply.github.com>
2026-06-26 23:41:51 +0530

    Added Module2 ECommerce Platform

---
+++ MODULE2/Ecommerce/Product.java
@@ -0,0 +1,47 @@
+public class Product {
+
+    int productId;
+    String productName;
+    String category;
+
+    public Product(int productId, String productName, String category) {
+        this.productId = productId;
+        this.productName = productName;
+        this.category = category;
+    }
+
+    public Product(int productId2, String productName2, int quantity, double price) {
+        // TODO Auto-generated constructor stub
+    }
+
+    public int getProductId() {
+        return productId;
+    }
+
+    public void setProductId(int productId) {
+        this.productId = productId;
+    }
+
+    public String getProductName() {
+        return productName;
+    }
+
+    public void setProductName(String productName) {
+        this.productName = productName;
+    }
+
+    public String getCategory() {
+        return category;
+    }
+
+    public void setCategory(String category) {
+        this.category = category;
+    }
+
+    @Override
+    public String toString() {
+        return "Product ID : " + productId +
+                "\nProduct Name : " + productName +
+                "\nCategory : " + category;
+    }
+}
